a8acceb56b506ad0ac1b5acbaaf7858d2ed73940
hyeongiii <hyeongiii0811@gmail.com>
2024-12-22 20:53:58 +0900

    :sparkles: feat: Auditing 클래스 생성

--- src/main/java/org/balllog/backend/BackendApplication.java
+++ src/main/java/org/balllog/backend/BackendApplication.java
@@ -1,8 +1,10 @@
-package org.example.backend;
+package org.balllog.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
